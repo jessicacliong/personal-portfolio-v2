ba1a11c5ff52b01d6b6dd98e0b215d747c9c28af
jessicacliong <14395@coderacademy.edu.au>
2025-02-12 21:11:23 +1100

    CSS Styling for Home Component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,47 +8,82 @@ import './home.css';
 
 const Home = () => {
   return (
-    <section className='home' id='home'>
-        <div className='home__container container'>
-            <p className="home__subtitle text-cs">
-                Hello, <span>My Name Is</span>
-            </p>
+     <section className='home' id='home'>
+          <div className='home__container container'>
+               <p className="home__subtitle text-cs">
+                    Hello, <span>My Name Is</span>
+               </p>
 
-            <h1 className='home__title text-cs'>
-                <span>JESSICA</span> LIONG
-            </h1>
+               <h1 className='home__title text-cs'>
+                    <span>JESSICA</span> LIONG
+               </h1>
 
-            <p className='home__job'>
-                <span className='text-cs'>I Am a</span> <b>Junior Web Developer</b>
-            </p>
+               <p className='home__job'>
+                    <span className='text-cs'>I Am a</span> <b>Junior Web Developer</b>
+               </p>
 
-            <p className='home__text'>
-               Enthusiastic professional transitioning careers to web development. 
-               Posses can-do attitude to problem-solving and learning, with a solid foundations in HTML, CSS, and JavaScript. 
-               Eager to leverage my skills to contribute to dynamic web development projects.
-            </p>
-            
-            <div className="home__socials">
-                <a href="" className="home__social-link">
-                    <FaLinkedin />
-                </a>
+               <div className="home__img-wrapper">
+                    <div className="home__banner">
+                         <img src={profileImg} alt="" className='home__profile' />
+                    </div>
 
-                <a href="" className="home__social-link">
-                    <FaGithubAlt />
-                </a>
 
-                <a href="" className="home__social-link">
-                    <FaTwitter />
-                </a>
-            </div>
+                    <p className="home__data home__data-one">
+                         <span className="text-lg">
+                         12 <b>+</b>
+                         </span>
 
-            <div className="home__btns">
-                <a href="" className="btn">Download CV</a>
-                <a href="" className="hero__link">My Skills</a>
-            </div>
-        </div>
-    </section>
+                         <span className="text-sm text-cs">
+                         Years of <span>Experience</span>
+                         </span>
+                    </p>
+
+
+                    <p className="home__data home__data-two">
+                         <span className="text-lg">
+                         330
+                         </span>
+                         
+                         <span className="text-sm text-cs">
+                         Completed <span>Projects</span>
+                         </span>
+                    </p>
+
+
+                    <img src={shapeOne} alt="" className="shape shape__1" />
+                    <img src={shapeTwo} alt="" className="shape shape__2" />
+                    <img src={shapeTwo} alt="" className="shape shape__3" />
+               </div>
+
+               <p className='home__text'>
+                    Enthusiastic professional transitioning careers to web development. 
+                    Posses can-do attitude to problem-solving and learning, with a solid foundations in HTML, CSS, and JavaScript. 
+                    Eager to leverage my skills to contribute to dynamic web development projects.
+               </p>
+               
+               <div className="home__socials">
+                    <a href="" className="home__social-link">
+                         <FaLinkedin />
+                    </a>
+
+                    <a href="" className="home__social-link">
+                         <FaGithubAlt />
+                    </a>
+
+                    <a href="" className="home__social-link">
+                         <FaTwitter />
+                    </a>
+               </div>
+
+               <div className="home__btns">
+                    <a href="" className="btn text-cs">
+                         Download CV
+                    </a>
+                    <a href="" className="hero__link text-cs">
+                         My Skills
+                    </a>
+               </div>
+          </div>
+     </section>
   )
 }
-
-export default Home
